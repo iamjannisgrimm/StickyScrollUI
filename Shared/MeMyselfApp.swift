5d5e8b94711c9cd43fe5d5a6cd7c0e0f15629ef9
Jannis Grimm <70415018+iamjannisgrimm@users.noreply.github.com>
2021-09-06 21:18:16 -0700

    Initial Commit

---
+++ Shared/MeMyselfApp.swift
@@ -0,0 +1,20 @@
+//
+//  MeMyselfApp.swift
+//  Shared
+//
+//  Created by Jannis Grimm on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MeMyselfApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
